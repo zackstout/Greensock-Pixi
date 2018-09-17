0e8eab2a5c19231bfa10369b88a25c6d559c4c73
Zack Stout <zackstout@gmail.com>
2018-09-17 11:43:49 -0500

    having some trouble removing elements, but flow feels better now

--- client.js
+++ client.js
@@ -59,15 +59,13 @@ function setup() {
         PIXI.loader.resources[all_imgs[2]].texture
     );
 
-    spWeather = new PIXI.Sprite(
-        PIXI.loader.resources[all_imgs[3]].texture
-    )
+
 
 
     sp1.alpha = 0;
     sp3.alpha = 0;
-    // spWeather.alpha = 0;
 
+    
     // Ok, there will be issues about resizing, like whether image is fatter or taller:
     sp1.height = window.innerHeight;
     sp2.height = window.innerHeight;
@@ -76,31 +74,48 @@ function setup() {
     app.stage.addChild(sp1);
     app.stage.addChild(sp2);
     app.stage.addChild(sp3);
-    app.stage.addChild(spWeather);
 
 
     var tl = new TimelineLite();
 
     const $grt = $('#greeting');
-    const $weather = $('#weather');
+    const $weather_cont = $('#weather')
+    const $weather = $('#weather_txt');
     const $news = $('#news');
 
 
-
-    // console.log(app.view);
-    // console.log(app.view.getContext('webgl')) // Oooh maybe just had to use webgl instead of 2d
-
+    // Need to be more thoughtful about how this relates to ICON:
     $weather.text('77')
     $grt.text('hi there');
 
+    tl.set($weather_cont, {width: 300})
 
-    tl.set(spWeather, {x: 400})
-    console.log(spWeather.x)
+    $im = $('<img>')
+        .attr('src', 'http://fridge.knife.screenfeed.com/content/weather-icons/basic/128/30.png')
+        .attr('height', '100px')
+        .attr('width', '100px')
+        // .attr('opacity', '0.8')
+    
+    // console.log($im)
+    $weather.prepend($im);
+
+    // Remember, need angle brackets here:
+    const $p = $('<p>').text('some news news news!')
+    $('#news_child').append($p);
+
+    const $p2 = $('<p>').text('ahoy hoy!')
+    tl.set($p2, {alpha: 0})
+
+
+
+    tl.set($p, {alpha: 0})
 
     // Set initial positions:
     tl.set($news, { x: 130, y: window.innerHeight - 100 })
     tl.set($news, {width: "500px"})
-    tl.set($weather, { x: spWeather.x + 40 , y: spWeather.y + 50, width: 150})
+
+    tl.set($weather_cont, {x: 500, y: 100})
+    // tl.set($weather, { x: spWeather.x + 40 , y: spWeather.y + 50, width: 150})
     tl.set($grt, { x: window.innerWidth / 2 - 50, y: window.innerHeight / 2 - 50 })
         // .to($grt, 1, { opacity: 0 })
         // .to($grt, 1, { opacity: 1 });
@@ -108,6 +123,7 @@ function setup() {
     tl.to($grt, 0.2, {opacity: 1})
     tl.to($grt, 0.8, {opacity: 0}, "+=1")
     tl.to($news, 0.8, {opacity: 1}, "+=0.2")
+    tl.set('#news_child', {width: 400})
 
     // tl.set($weather)
     tl.to($weather, 0.6, {opacity: 1}, "+=0.4")
@@ -115,10 +131,13 @@ function setup() {
     // tl.to($news, 1.3, {css: {height: "100px"}})
     // tl.to($news, 1.3, {y: "+=100px"}, '-=1')
     tl.fromTo("#news_child", 0.8, {height: "0px"}, {height: "100px"})
+
+    tl.to($p, 0.5, {alpha: 1})
     // tl.fromTo($news, 1.3, {scaleY: 0}, {scaleY: 1})
 
     // We need this text to NOT show up until the time is right:
-    $("#news_child").text('This is very important news!!!!')
+    // $("#news_child").text('text')
+    
 
 
 
@@ -126,8 +145,23 @@ function setup() {
 
 
     // Two changes/cycles:
-    tl.to(sp2, 0.6, { alpha: 0 }, "+=2"); // Wait 2 seconds before starting.
+    tl.to($p, 0.6, {alpha: 0}, "+=2")
+
+    tl.to(sp2, 0.6, { alpha: 0 }, "+=0.3"); // Wait 2 seconds before starting.
     tl.to(sp3, 0.6, { alpha: 1 }, "-=0.5");
+
+
+    // we need to NOT REMOVE IT until we get here!!!
+    // If we don't remove it, next one gets pushed too far down....
+    // $p.remove();
+    $('#news_child').append($p2);
+    tl.set($p2, {y: "-30px"})
+    tl.set($p, {height: "0px"})
+
+    // console.log(tl.getChildren())
+    tl.to($p2, 0.6, {alpha: 1});
+
+    
 
     tl.to(sp3, 0.6, { alpha: 0 }, "+=2"); // Wait 2 seconds before starting.
     tl.to(sp1, 0.6, { alpha: 1 }, "-=0.5");
