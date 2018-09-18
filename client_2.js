4b79be9044e372eed6ba339eb90d51f2f160eba0
Zack Stout <zackstout@gmail.com>
2018-09-18 13:15:47 -0500

    fairly ugly implementation of blue sidebar

--- client_2.js
+++ client_2.js
@@ -1,7 +1,7 @@
 
 // =======================================================================================
 
-let data, app, imgs, titles, tl, sprites, $grt, $weather_cont, $news, $news_text;
+let weather_data, app, imgs, titles, tl, sprites, $grt, $weather_cont, $news, $news_text, $blue_span;
 let all_titles = [];
 let w, h;
 
@@ -26,7 +26,6 @@ $(document).ready(function() {
   app.renderer.backgroundColor = 0x061639;
   app.renderer.view.style.position = "absolute";
   app.renderer.view.style.display = "block";
-  // app.renderer.view.style.padding = '-100px 0 0 0 ';
   app.renderer.autoResize = true;
   app.renderer.resize(w, h);
 });
@@ -42,16 +41,11 @@ async function prepareData() {
     articles = await $.get(news_url);
     weather = await $.get(weather_url);
 
-    data = {
-      articles: [],
-      weather: {}
-    };
-
-    data.weather.temp = weather.Locations[0].WeatherItems[0].CurrentTempF;
-    data.weather.icon = weather.Locations[0].WeatherItems[0].IconURL;
-    data.articles = articles.Items.map(a => ({title: a.Title, img: a.Media[0].Url}));
-    imgs = data.articles.map(a => a.img);
-    titles = data.articles.map(a => a.title);
+    weather_data = {}; // Def need this
+    weather_data.temp = weather.Locations[0].WeatherItems[0].CurrentTempF;
+    weather_data.icon = weather.Locations[0].WeatherItems[0].IconURL;
+    imgs = articles.Items.map(a => a.Media[0].Url);
+    titles = articles.Items.map(a => a.Title);
 
     PIXI.loader
     .add(imgs)
@@ -91,20 +85,97 @@ function beginAnimation() {
   tl.to($grt, 0.2, {alpha: 1}, "+=1");
   tl.to($grt, 1, {alpha: 0}, "+=1");
 
-  // First image:
+  // Bring in first image:
   tl.to(sprites[0], 1, {alpha: 1}, "+=0.5");
 
   // Bring in weather:
-  tl.to($weather_cont, 1, {alpha: 0.8}, "+=0.5"); // only bring alpha up to 0.8
+  tl.to($weather_cont, 1, {alpha: 0.85}, "+=0.5"); // only bring alpha up to 0.9
 
   // Bring in news:
   tl.fromTo("#news_child", 0.8, {height: "0px"}, {height: "100px"});
+  tl.to($blue_span, 0.2, {"background-color": "blue"}, "-=0.7")
+  tl.to($blue_span, 0.2, {alpha: 1, height: 0}, "-=0.8");
+
+  // tl.fromTo($blue_span, 0.8, {height: "0px"}, {height: "100px"});
+  tl.fromTo($blue_span, 0.6, {css: {"padding-bottom": "0px"}}, {css: {"padding-bottom": "65px"}}, "-=0.6")
+
   tl.to($news_text, 0.5, {alpha: 1});
 
   // Build the animation timeline:
-  for (let i=1; i < 10; i++) {
+  for (let i=1; i < 14; i++) {
     addToTimeline(i);
   }
+}
+
+// =======================================================================================
+
+function addToTimeline(i) {
+  // Fade out title:
+  tl.to($news_text, 0.6, {alpha: 0}, "+=3");
+
+  // Cross-fade background:
+  tl.to(sprites[0], 0.6, {alpha: 0}, "-=0.2"); // "-=0.2" syncs up fading of title well with changing background.
+  tl.to(sprites[1], 0.6, {alpha: 1}, "-=1.0"); // Difference between this time-offset and previous time-offset should be about 0.8 to properly align with .
+
+  // Replace and fade in new title:
+  tl.to($news_text, 0.1, {text: titles[i % titles.length]}, "-=0.3"); // Make them spawn a bit faster with "-=0.3". Get title for any i with % titles.length.
+  tl.to($news_text, 0.6, {alpha: 1})
+
+  // Move head of sprites array to tail:
+  let first_sprite = sprites.splice(0, 1);
+  sprites.push(first_sprite);
+}
+
+// =======================================================================================
+
+function initializeGreeting() {
+  $grt.text('News & Weather');
+  tl.set($grt, { x: w / 2 - 100, y: h / 2 - 50, width: 300 }); // Because [100, 50] are roughly dimensions of the greetings Div
+  tl.set($grt, {alpha: 0});
+}
+
+
+function initializeWeather() {
+  // Create image:
+  $im = $('<img>')
+  .attr('src', weather_data.icon)
+  .attr('height', '100px')
+  .attr('width', '100px');
+  tl.set($im, {css: { "padding-top": "20px", "padding-left": "20px"}}); // This one *doesn't* need absolute position..
+
+  // Create text:
+  const $deg = $("<span>").html('&deg;'); // Create as own element so we can shift it left.
+  tl.set($deg, {css: {"padding-right": "-5px"}})
+  const $temp = $("<span>").html(weather_data.temp);
+  $temp.append($deg);
+
+  tl.set($temp, {css: {"position": "absolute", "padding-top": "25px", "padding-left": "30px"}});
+  tl.set($temp, {left: 100, width: 100})
+
+  // Add both to DOM:
+  $weather_cont.append($temp);
+  $weather_cont.prepend($im);
+
+  tl.set($weather_cont, {left: w - 250, alpha: 0, top: 50, width: 230})
+}
+
+
+function initializeNews() {
+  var title = titles[0];
+  $news_text = $('<p>').text(title);
+
+  $blue_span = $('<span>').html('');
+  tl.set($blue_span, {css: {"padding-right": "20px", "opacity": 0}})
+  // Not sure we need this rigamarole anymore:
+  $('#news_child').append($blue_span).append($news_text);
+  tl.set($news_text, {alpha: 0, height: 0});
+
+  // Set initial positions:
+  tl.set($news, { x: 50, y: h - 50 });
+  tl.set('#news_child', { width: w - 110 }); // needed
+
+  tl.set($news_text, { css: {"position": "relative"}})
+  tl.set($news_text, {left: 50, top: -50})
 }
 
 // =======================================================================================
@@ -122,76 +193,6 @@ function resize(sprite) {
   sprite.scale.x = ratio;
   sprite.scale.y = ratio;
 }
-
-// =======================================================================================
-
-function addToTimeline(i) {
-  // Fade out title:
-  tl.to($news_text, 0.6, {alpha: 0}, "+=3");
-
-  // Cross-fade background:
-  tl.to(sprites[0], 0.6, {alpha: 0}, "-=0.2"); // "-=0.2" syncs up fading of title well with changing background.
-  tl.to(sprites[1], 0.6, {alpha: 1}, "-=1.0"); // Difference between this time-offset and previous time-offset should be about 0.8 to properly align with .
-
-  // Replace and fade in new title:
-  tl.to($news_text, 0.1, {text: titles[i]}, "-=0.3"); // Make them spawn a bit faster with "-=0.3".
-  tl.to($news_text, 0.6, {alpha: 1})
-
-  sprites.splice(0, 1);
-}
-
-// =======================================================================================
-
-function initializeGreeting() {
-  $grt.text('News & Weather');
-  tl.set($grt, { x: w / 2 - 100, y: h / 2 - 50 }); // Because [100, 50] are roughly dimensions of the greetings Div
-  tl.set($grt, {alpha: 0});
-}
-
-
-function initializeWeather() {
-  // Create image:
-  $im = $('<img>')
-  .attr('src', `${data.weather.icon}`)
-  .attr('height', '100px')
-  .attr('width', '100px');
-  // tl.set($im, {css: { "padding-top": "10px", "position": "absolute"}});
-  tl.set($im, {css: { "padding-top": "20px", "padding-left": "20px"}});
-
-  // Create text:
-  const $temp = $("<span>").html(`${data.weather.temp} &deg;`);
-  // tl.set($temp, {css: {"position": "absolute", "padding-top": "20px"}});
-  tl.set($temp, {css: {"position": "absolute", "padding-top": "25px", "padding-left": "30px"}});
-
-  tl.set($temp, {left: 100, width: 100})
-
-  // Add both to DOM:
-  $weather_cont.append($temp);
-  $weather_cont.prepend($im);
-
-  tl.set($weather_cont, {left: w - 250, alpha: 0, top: 100, width: 230})
-}
-
-
-function initializeNews() {
-  var title = titles[0];
-  $news_text = $('<p>').text(title);
-  // Not sure we need this rigamarole anymore:
-  $('#news_child').append($news_text);
-  tl.set($news_text, {alpha: 0});
-
-  // Set initial positions:
-  tl.set($news, { x: 50, y: h - 100 });
-  // HMm we need to contain the text within this region as well:
-  tl.set('#news_child', { width: w - 60 }); // needed
-
-  tl.set($news_text, { css: {"position": "relative"}})
-  tl.set($news_text, {left: 60, top: -15})
-}
-
-// =======================================================================================
-
-
 
 
 
