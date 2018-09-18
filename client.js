456557bd44dc30858634f272a5522cf3c696d3f0
Zack Stout <zackstout@gmail.com>
2018-09-17 20:44:06 -0500

    finally figured out how to change text with timeline..almost there now

--- client.js
+++ client.js
@@ -1,210 +1,191 @@
+let app, all_imgs;
 
-// =======================================================================================
-let data, app, imgs, titles, tl, sprites, $grt, $weather_cont, $news, $news_text;
-let all_titles = [];
+window.onload = function () {
+    all_imgs = ['styles/jmoney.jpg', 'styles/pics_hugh.jpg', 'styles/osprey.jpg', 'http://fridge.knife.screenfeed.com/content/weather-icons/basic/128/30.png'];
 
-$(document).ready(function() {
-  $grt = $('#greeting');
-  $weather_cont = $('#weather');
-  $news = $('#news');
+    // NOTE: Instead of using a loop, we could use a recursive (call yourself when you're done) function to handle the cycling. 
 
-  // Kick things off:
-  prepareData();
+    // NOTE: Maybe we need a CONTAINER???
 
-  tl = new TimelineLite();
-  app = new PIXI.Application();
-  // creates a CANVAS element:
-  document.body.appendChild(app.view);
+    // Nice, so we can still use async/await to good effect:
+    $.get('https://files-mihchsciio.now.sh/demo-project.json')
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-  // From Pixi docs tutorial:
-  app.renderer.backgroundColor = 0x061639;
-  app.renderer.view.style.position = "absolute";
-  app.renderer.view.style.display = "block";
-  app.renderer.autoResize = true;
-  app.renderer.resize(window.innerWidth, window.innerHeight);
-});
+    app = new PIXI.Application();
+    // creates a CANVAS element:
+    document.body.appendChild(app.view);
 
-// =======================================================================================
-async function prepareData() {
-  let articles, weather;
-  try {
-    articles = await $.get('https://files-mihchsciio.now.sh/demo-project.json');
-    weather = await $.get('http://kitchen.screenfeed.com/weather/v2/data/p060kk4gfpvsphh415mnj7547c.json?current=true');
-  } catch {
-    console.log('uh oh');
-  }
+    // from pixi docs tutorial:
+    app.renderer.backgroundColor = 0x061639;
+    app.renderer.view.style.position = "absolute";
+    app.renderer.view.style.display = "block";
+    app.renderer.autoResize = true;
+    app.renderer.resize(window.innerWidth, window.innerHeight);
 
-  data = {
-    articles: [],
-    weather: {}
-  };
-
-  data.weather.temp = weather.Locations[0].WeatherItems[0].CurrentTempF;
-  data.weather.icon = weather.Locations[0].WeatherItems[0].IconURL;
-  data.articles = articles.Items.map(a => ({title: a.Title, img: a.Media[0].Url}));
-  imgs = data.articles.map(a => a.img);
-  titles = data.articles.map(a => a.title);
-
-  PIXI.loader
-  .add(imgs)
-  // Kick off next step:
-  .load(setup);
-
-  console.log(data);
+    PIXI.loader
+        .add(all_imgs)
+        .load(setup);
 }
 
-// =======================================================================================
-// Could this ever run before data is loaded? No, because we're awaiting.
+// First step: Zip up all sprites and titles into an array of objects.
+
 function setup() {
-  console.log('setting up');
-  sprites = imgs.map(i => new PIXI.Sprite(PIXI.loader.resources[i].texture));
 
-  sprites.forEach(s => {
-    app.stage.addChild(s);
-    resize(s);
-    tl.set(s, {alpha: 0});
-  });
+    sp1 = new PIXI.Sprite(
+        PIXI.loader.resources[all_imgs[0]].texture
+    );
 
-  beginAnimation();
+    sp2 = new PIXI.Sprite(
+        PIXI.loader.resources[all_imgs[1]].texture
+    );
+
+    sp3 = new PIXI.Sprite(
+        PIXI.loader.resources[all_imgs[2]].texture
+    );
+
+    console.log(sp1.scale)
+
+    // Ok this will work:
+    // We can get height and width of each photo and then figure out how much it needs to be scaled.
+    sp1.scale.x = 1.5;
+    sp1.scale.y = 1.5;
+
+    sp1.alpha = 0;
+    sp3.alpha = 0;
+
+
+    app.stage.addChild(sp1);
+    app.stage.addChild(sp2);
+    app.stage.addChild(sp3);
+
+
+    var tl = new TimelineLite();
+
+
+    const $grt = $('#greeting');
+    const $weather_cont = $('#weather')
+    const $news = $('#news');
+
+    const $temp = $("<span>").html('77 &deg;')
+    $weather_cont.append($temp)
+
+    tl.set($temp, {css: {"position": "absolute", "padding-top": "25px"}})
+
+    $grt.text('News & Weather');
+
+    tl.set($weather_cont, {width: 190, height: 100})
+
+    $im = $('<img>')
+        .attr('src', 'http://fridge.knife.screenfeed.com/content/weather-icons/basic/128/30.png')
+        .attr('height', '100px')
+        .attr('width', '100px')
+        // .attr('opacity', '0.8')
+    
+    tl.set($im, {css: { "padding-top": "10px"}})
+
+    // console.log($im)
+    $weather_cont.prepend($im);
+
+    // Remember, need angle brackets here:
+    const $p = $('<p>').text('some news news news!')
+    .css('background-color', 'blue')
+    // .css('padding', '0px')
+    $('#news_child').append($p);
+
+    // tl.set('#news_child', {left: "100px"})
+    
+    tl.set($weather_cont, {alpha: 0})
+
+
+
+    const $p2 = $('<p>').text('ahoy hoy!')
+    tl.set($p2, {alpha: 0})
+    const $p3 = $('<p>').text('well well well!')
+    tl.set($p3, {alpha: 0})
+    const $p4 = $('<p>').text('you know me!')
+    tl.set($p4, {alpha: 0})
+    const $p5 = $('<p>').text('i\'m just tryna...!')
+    tl.set($p5, {alpha: 0})
+
+
+    tl.set($p, {alpha: 0})
+
+    // Set initial positions:
+    tl.set($news, { x: 130, y: window.innerHeight - 100 })
+    tl.set($news, {width: "500px"})
+
+    tl.set($weather_cont, {x: window.innerWidth - 200, y: 100})
+    // tl.set($weather, { x: spWeather.x + 40 , y: spWeather.y + 50, width: 150})
+    tl.set($grt, { x: window.innerWidth / 2 - 50, y: window.innerHeight / 2 - 50 })
+        // .to($grt, 1, { opacity: 0 })
+        // .to($grt, 1, { opacity: 1 });
+
+    tl.to($grt, 0.2, {opacity: 1})
+    tl.to($grt, 0.8, {opacity: 0}, "+=1")
+    tl.to($news, 0.8, {opacity: 1}, "+=0.2")
+    tl.set('#news_child', {width: 400})
+    // tl.set('#news', {height: 0})
+
+    tl.to($weather_cont, 0.6, {opacity: 1}, "+=0.4")
+
+
+    // This is why we wanted to put it in a parent, to get this to work:
+    tl.fromTo("#news_child", 0.8, {height: "0px"}, {height: "100px"})
+
+    tl.to($p, 0.5, {alpha: 1})
+    // tl.fromTo($news, 1.3, {scaleY: 0}, {scaleY: 1})
+
+ 
+
+
+    // Two changes/cycles:
+    tl.to($p, 0.6, {alpha: 0}, "+=2")
+
+    tl.to(sp2, 0.6, { alpha: 0 }, "+=0.3"); // Wait 2 seconds before starting.
+    tl.to(sp3, 0.6, { alpha: 1 }, "-=0.5");
+
+
+    // we need to NOT REMOVE IT until we get here!!!
+    // If we don't remove it, next one gets pushed too far down....
+    // $p.remove();
+    $('#news_child').append($p2);
+    tl.set($p2, {y: "-30px"}) // Feels bad to have to hard code this 30
+    tl.set($p, {height: "0px"}) // This whole bit is so ugly
+    tl.to($p2, 0.6, {alpha: 1});
+
+    
+
+    tl.to(sp3, 0.6, { alpha: 0 }, "+=2"); // Wait 2 seconds before starting.
+    tl.to(sp1, 0.6, { alpha: 1 }, "-=0.5");
+
+    // Maybe the idea is to load all text onto page, similar with starting images...
+
+    $('#news_child').append($p3);
+    tl.set($p3, {y: "-60px"}) // Feels bad to have to hard code this 30 
+    // -- The real problem is going to be accounting for possible multi-line titles....
+    tl.set($p2, {height: 0, alpha: 0})
+    tl.to($p3, 0.6, {alpha: 1});
+
+
+
+
+    tl.to(sp1, 0.6, { alpha: 0 }, "+=2"); // Wait 2 seconds before starting.
+    tl.to(sp2, 0.6, { alpha: 1 }, "-=0.5");
+
+    $('#news_child').append($p4);
+    tl.set($p4, {y: "-90px"}) // Feels bad to have to hard code this 30 
+    // -- The real problem is going to be accounting for possible multi-line titles....
+    tl.set($p3, {height: 0, alpha: 0})
+    tl.to($p4, 0.6, {alpha: 1});
+
+
+
+    tl.to(sp2, 0.6, { alpha: 0 }, "+=2"); // Wait 2 seconds before starting.
+    tl.to(sp3, 0.6, { alpha: 1 }, "-=0.5");
+
 }
-
-
-// =======================================================================================
-function beginAnimation() {
-
-  initializeGreeting();
-  initializeWeather();
-  initializeNews();
-
-  // Animate greeting:
-  tl.to($grt, 0.2, {alpha: 1}, "+=1");
-  tl.to($grt, 1, {alpha: 0}, "+=1");
-
-  // First image:
-  tl.to(sprites[0], 1, {alpha: 1}, "+=0.5");
-
-  // Bring in weather:
-  tl.to($weather_cont, 1, {alpha: 1}, "+=0.5");
-
-  // Bring in news:
-  tl.fromTo("#news_child", 0.8, {height: "0px"}, {height: "100px"});
-  tl.to($news_text, 0.5, {alpha: 1});
-
-
-  all_titles.push($news_text) // necessary to get first one in the array
-  for (let i=0; i < 10; i++) {
-    addToDom(i);
-  }
-
-  for (let i=1; i < 10; i++) {
-    addToTimeline(i);
-  }
-
-  console.log('news txt is...', $news_text[0].childNodes);
-}
-
-
-
-// =======================================================================================
-function resize(sprite) {
-  // Duh, use width and height, not vertexData....strange it behaves differently though, vis a vis asynchronicity.
-  const hor_dist = sprite.width;
-  const vert_dist = sprite.height;
-
-  // Whichever is bigger is the direction we need to switch:
-  const hor_diff = window.innerWidth - hor_dist;
-  const vert_diff = window.innerHeight - vert_dist;
-
-  const ratio = (hor_diff > vert_diff) ? window.innerWidth / hor_dist : window.innerHeight / vert_dist;
-  sprite.scale.x = ratio;
-  sprite.scale.y = ratio;
-}
-
-function addToDom(i) {
-  console.log(titles);
-  const $new_title = $('<p>').text(titles[i]);
-  // tl.set($new_title, {alpha: 0});
-  $news_text.append($new_title);
-  all_titles.push($new_title);
-}
-
-function addToTimeline(i) {
-  // This won't work, because loop runs every time, and we just end up with last title:
-  // $news_text.text(title);
-
-  // const allTitles = $news_text[0].childNodes;
-
-  console.log(all_titles);
-  tl.to(all_titles[0], 0.6, {alpha: 0}, "+=1"); // hmm not working
-
-  tl.to(sprites[0], 0.6, {alpha: 0}, "+=0.3");
-  tl.to(sprites[1], 0.6, {alpha: 1}, "-=0.5");
-
-  tl.to(all_titles[2], 0.6, {alpha: 1})
-
-
-  // tl.to($p, 0.6, {alpha: 0}, "+=2")
-  //
-  //   tl.to(sp2, 0.6, { alpha: 0 }, "+=0.3"); // Wait 2 seconds before starting.
-  //   tl.to(sp3, 0.6, { alpha: 1 }, "-=0.5");
-  //
-  //
-  //   // we need to NOT REMOVE IT until we get here!!!
-  //   // If we don't remove it, next one gets pushed too far down....
-  //   // $p.remove();
-  //   $('#news_child').append($p2);
-  //   tl.set($p2, {y: "-30px"}) // Feels bad to have to hard code this 30
-  //   tl.set($p, {height: "0px"}) // This whole bit is so ugly
-  //   tl.to($p2, 0.6, {alpha: 1});
-
-
-
-
-}
-
-// =======================================================================================
-function initializeGreeting() {
-  $grt.text('News & Weather');
-  tl.set($grt, { x: window.innerWidth / 2 - 100, y: window.innerHeight / 2 - 50 }); // Because [100, 50] are roughly dimensions of the greetings Div
-  tl.set($grt, {alpha: 0});
-}
-
-
-function initializeWeather() {
-  $im = $('<img>')
-  .attr('src', 'http://fridge.knife.screenfeed.com/content/weather-icons/basic/128/30.png')
-  .attr('height', '100px')
-  .attr('width', '100px');
-
-  const $temp = $("<span>").html('77 &deg;');
-  $weather_cont.append($temp);
-
-  tl.set($temp, {css: {"position": "absolute", "padding-top": "20px"}});
-  tl.set($temp, {left: 100, width: 100})
-  tl.set($im, {css: { "padding-top": "10px", "position": "absolute"}});
-  $weather_cont.prepend($im);
-
-  tl.set($weather_cont, {left: window.innerWidth - 220, alpha: 0})
-
-}
-
-
-function initializeNews() {
-  var title = "U.S. union urges states to look into T-Mobile purchase of Sprint";
-  $news_text = $('<p>').text(title);
-  $('#news_child').append($news_text);
-  tl.set($news_text, {alpha: 0});
-
-  // Set initial positions:
-  tl.set($news, { x: 130, y: window.innerHeight - 100 });
-  tl.set('#news_child', {width: window.innerWidth - 150});
-}
-
-// =======================================================================================
-
-
-
-
-
-
-//
--- client_2.js
+++ client_2.js
@@ -1,11 +1,17 @@
 
 // =======================================================================================
-let data, app, imgs, tl, sprites, $grt, $weather_cont, $news;
+
+let data, app, imgs, titles, tl, sprites, $grt, $weather_cont, $news, $news_text;
+let all_titles = [];
+let w, h;
 
 $(document).ready(function() {
   $grt = $('#greeting');
   $weather_cont = $('#weather');
   $news = $('#news');
+
+  w = window.innerWidth;
+  h = window.innerHeight;
 
   // Kick things off:
   prepareData();
@@ -20,35 +26,37 @@ $(document).ready(function() {
   app.renderer.view.style.position = "absolute";
   app.renderer.view.style.display = "block";
   app.renderer.autoResize = true;
-  app.renderer.resize(window.innerWidth, window.innerHeight);
+  app.renderer.resize(w, h);
 });
 
 // =======================================================================================
+
 async function prepareData() {
   let articles, weather;
   try {
     articles = await $.get('https://files-mihchsciio.now.sh/demo-project.json');
     weather = await $.get('http://kitchen.screenfeed.com/weather/v2/data/p060kk4gfpvsphh415mnj7547c.json?current=true');
+
+    data = {
+      articles: [],
+      weather: {}
+    };
+
+    data.weather.temp = weather.Locations[0].WeatherItems[0].CurrentTempF;
+    data.weather.icon = weather.Locations[0].WeatherItems[0].IconURL;
+    data.articles = articles.Items.map(a => ({title: a.Title, img: a.Media[0].Url}));
+    imgs = data.articles.map(a => a.img);
+    titles = data.articles.map(a => a.title);
+
+    PIXI.loader
+    .add(imgs)
+    // Kick off next step:
+    .load(setup);
+
   } catch {
-    console.log('uh oh');
+    console.log('Failed to grab JSON.');
+    throw new Error('Failed to grab JSON.'); // would this work?
   }
-
-  data = {
-    articles: [],
-    weather: {}
-  };
-
-  data.weather.temp = weather.Locations[0].WeatherItems[0].CurrentTempF;
-  data.weather.icon = weather.Locations[0].WeatherItems[0].IconURL;
-  data.articles = articles.Items.map(a => ({title: a.Title, img: a.Media[0].Url}));
-  imgs = data.articles.map(a => a.img);
-
-  PIXI.loader
-  .add(imgs)
-  // Kick off next step:
-  .load(setup);
-
-  console.log(data);
 }
 
 // =======================================================================================
@@ -68,6 +76,7 @@ function setup() {
 
 
 // =======================================================================================
+
 function beginAnimation() {
 
   initializeGreeting();
@@ -82,38 +91,103 @@ function beginAnimation() {
   tl.to(sprites[0], 1, {alpha: 1}, "+=0.5");
 
   // Bring in weather:
-
+  tl.to($weather_cont, 1, {alpha: 1}, "+=0.5");
 
   // Bring in news:
   tl.fromTo("#news_child", 0.8, {height: "0px"}, {height: "100px"});
-  tl.to($p, 0.5, {alpha: 1});
+  tl.to($news_text, 0.5, {alpha: 1});
 
 
+  all_titles.push($news_text) // necessary to get first one in the array
+  for (let i=0; i < 10; i++) {
+    addTitleToDom(i);
+  }
+
+  // Huh.... Why isn't this creating a loop of all the images?
+  // Oh duh because we're not splicing the array yet.
+  for (let i=1; i < 10; i++) {
+    addToTimeline(i);
+  }
+
+  console.log('news txt is...', $news_text[0].childNodes[1]);
 }
 
-
-
 // =======================================================================================
+
 function resize(sprite) {
   // Duh, use width and height, not vertexData....strange it behaves differently though, vis a vis asynchronicity.
   const hor_dist = sprite.width;
   const vert_dist = sprite.height;
 
-  // Whichever is bigger is the direction we need to switch:
-  const hor_diff = window.innerWidth - hor_dist;
-  const vert_diff = window.innerHeight - vert_dist;
+  // Whichever is bigger is the direction we need to stretch:
+  const hor_diff = w - hor_dist;
+  const vert_diff = h - vert_dist;
 
-  const ratio = (hor_diff > vert_diff) ? window.innerWidth / hor_dist : window.innerHeight / vert_dist;
+  const ratio = (hor_diff > vert_diff) ? w / hor_dist : h / vert_dist;
   sprite.scale.x = ratio;
   sprite.scale.y = ratio;
 }
 
+// =======================================================================================
 
+function addTitleToDom(i) {
+  // console.log(titles);
+  const $new_title = $('<p>').text(titles[i]);
+  // tl.set($new_title, {alpha: 0});
+  $news_text.append($new_title);
+  all_titles.push($new_title);
+}
 
 // =======================================================================================
+
+function addToTimeline(i) {
+  // This won't work, because loop runs every time, and we just end up with last title:
+  // $news_text.text(title);
+
+  // const allTitles = $news_text[0].childNodes;
+
+  // console.log(all_titles);
+
+  // Fade out title:
+  tl.to(all_titles[0], 0.6, {alpha: 0}, "+=3");
+
+  // Cross-fade background:
+  tl.to(sprites[0], 0.6, {alpha: 0}, "+=0.3");
+  tl.to(sprites[1], 0.6, {alpha: 1}, "-=0.5");
+
+  // Could loop through and set all alpha to 0 if not the current title.
+  // for (let j=0; j < all_titles.length; j++) {
+  //   if (j!==i) {
+  //     tl.set(all_titles[j], {alpha: 0})
+  //   }
+  // }
+
+  // Fade in new title:
+  const offset = 30 * i;
+  // tl.set(all_titles[0], {y: `-=${offset}`})
+  // console.log(all_titles[2][0].innerHTML);
+
+  tl.to(all_titles[0], 0.2, {text: all_titles[2][0].innerHTML})
+
+  tl.to(all_titles[0], 0.6, {alpha: 1})
+
+  sprites.splice(0, 1);
+
+
+
+  // if (i == 1) all_titles.splice(0, 1);
+  // all_titles.splice(0, 2);
+  // all_titles.splice(0, 1);
+
+
+
+}
+
+// =======================================================================================
+
 function initializeGreeting() {
   $grt.text('News & Weather');
-  tl.set($grt, { x: window.innerWidth / 2 - 100, y: window.innerHeight / 2 - 50 }); // Because [100, 50] are roughly dimensions of the greetings Div
+  tl.set($grt, { x: w / 2 - 100, y: h / 2 - 50 }); // Because [100, 50] are roughly dimensions of the greetings Div
   tl.set($grt, {alpha: 0});
 }
 
@@ -124,7 +198,7 @@ function initializeWeather() {
   .attr('height', '100px')
   .attr('width', '100px');
 
-  const $temp = $("<span>").html('77 &deg;');
+  const $temp = $("<span>").html(`${data.weather.temp} &deg;`);
   $weather_cont.append($temp);
 
   tl.set($temp, {css: {"position": "absolute", "padding-top": "20px"}});
@@ -132,19 +206,19 @@ function initializeWeather() {
   tl.set($im, {css: { "padding-top": "10px", "position": "absolute"}});
   $weather_cont.prepend($im);
 
-  tl.set($weather_cont, {left: window.innerWidth - 220})
+  tl.set($weather_cont, {left: w - 220, alpha: 0})
 }
 
 
 function initializeNews() {
   var title = "U.S. union urges states to look into T-Mobile purchase of Sprint";
-  const $p = $('<p>').text(title);
-  $('#news_child').append($p);
-  tl.set($p, {alpha: 0});
+  $news_text = $('<p>').text(title);
+  $('#news_child').append($news_text);
+  tl.set($news_text, {alpha: 0});
 
   // Set initial positions:
-  tl.set($news, { x: 130, y: window.innerHeight - 100 });
-  tl.set('#news_child', {width: window.innerWidth - 150});
+  tl.set($news, { x: 130, y: h - 100 });
+  tl.set('#news_child', {width: w - 150});
 }
 
 // =======================================================================================
